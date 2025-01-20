2534d2f881f29e6ce6863fab12f6a2fedc1aa9f3
kukabi <kutsalbilgin@gmail.com>
2025-01-20 19:08:50 +0300

    Dependency updates. GitHub action improvement.

--- _chainspecs/init.sh
+++ _chainspecs/init.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # coretime westend
+cd "$(dirname "$0")"
 wget https://github.com/paritytech/polkadot-sdk/raw/refs/heads/master/cumulus/parachains/chain-specs/coretime-westend.json
