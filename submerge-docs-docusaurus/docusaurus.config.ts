c2ed19c5cb6ac0ca713b237beb6ddf754b0d9340
kukabi <kutsalbilgin@gmail.com>
2025-08-08 16:00:02 +0300

    Add docusaurus and mintlify docs.

---
+++ submerge-docs-docusaurus/docusaurus.config.ts
@@ -0,0 +1,171 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+import type * as Plugin from '@docusaurus/types/src/plugin';
+import type * as OpenApiPlugin from 'docusaurus-plugin-openapi-docs';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+    title: 'Submerge Developer Documentation',
+    tagline: 'Blockchain data simplified',
+    favicon: 'img/submerge_icon_circular.png',
+
+    // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+    future: {
+        v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    },
+
+    // Set the production url of your site here
+    url: 'https://developer.submerge.io',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl: 'https://github.com/helikon-labs/submerge/tree/main/submerge-docs/',
+                    docItemComponent: '@theme/ApiItem',
+                    routeBasePath: '/',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        navbar: {
+            title: 'Developer',
+            logo: {
+                alt: 'Submerge Developer Documentation',
+                src: 'img/logo.svg',
+            },
+            items: [
+                {
+                    type: 'doc',
+                    label: 'Introduction',
+                    docId: 'introduction',
+                    position: 'left',
+                },
+                {
+                    label: 'Petstore API',
+                    position: 'left',
+                    to: '/petstore-api',
+                },
+                {
+                    label: 'Placeholder API',
+                    position: 'left',
+                    to: '/placeholder-api',
+                },
+                {
+                    href: 'https://github.com/helikon-labs/submerge',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Tutorial',
+                            to: '/docs/introduction',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/docusaurus',
+                        },
+                        {
+                            label: 'X',
+                            href: 'https://x.com/docusaurus',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: '/blog',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/facebook/docusaurus',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+
+    plugins: [
+        [
+            'docusaurus-plugin-openapi-docs',
+            {
+                id: 'openapi',
+                docsPluginId: 'classic',
+                config: {
+                    petstore: {
+                        specPath: 'spec/petstore.yaml',
+                        outputDir: 'docs/petstore',
+                        downloadUrl:
+                            'https://raw.githubusercontent.com/PaloAltoNetworks/docusaurus-template-openapi-docs/main/examples/petstore.yaml',
+                        sidebarOptions: {
+                            groupPathsBy: 'tag',
+                            categoryLinkSource: 'tag',
+                        },
+                    } satisfies OpenApiPlugin.Options,
+                    placeholder: {
+                        specPath: 'spec/placeholder.yaml',
+                        outputDir: 'docs/placeholder',
+                        sidebarOptions: {
+                            groupPathsBy: 'tag',
+                            categoryLinkSource: 'tag',
+                        },
+                    } satisfies OpenApiPlugin.Options,
+                } satisfies Plugin.PluginOptions,
+            },
+        ],
+    ],
+    themes: ['docusaurus-theme-openapi-docs'],
+};
+
+export default config;
