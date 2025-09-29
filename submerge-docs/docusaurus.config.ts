2ff243ca50e0a87b4e5ca186035981f4ccfe56bc
kukabi <kutsalbilgin@gmail.com>
2025-09-29 12:37:02 +0300

    Fix Docusaurus issue.

--- submerge-docs/docusaurus.config.ts
+++ submerge-docs/docusaurus.config.ts
@@ -127,8 +127,8 @@ const config: Config = {
                     title: 'Docs',
                     items: [
                         {
-                            label: 'Tutorial',
-                            to: '/docs/introduction',
+                            label: 'Crystal API',
+                            to: '/crystal-api',
                         },
                     ],
                 },
@@ -152,10 +152,6 @@ const config: Config = {
                 {
                     title: 'More',
                     items: [
-                        {
-                            label: 'Blog',
-                            to: '/blog',
-                        },
                         {
                             label: 'GitHub',
                             href: 'https://github.com/facebook/docusaurus',
