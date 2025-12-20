25c1cd2fc8704b18632740b1fc922fd7c4264138
kukabi <kutsalbilgin@gmail.com>
2025-12-20 18:57:21 +0300

    Fix clap args parsing logic.

--- submerge-crystal/src/main.rs
+++ submerge-crystal/src/main.rs
@@ -7,10 +7,7 @@ use tracing::{span, Level};
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let _tracing_span = span!(Level::TRACE, "Submerge Crystal");
-    let args = match Args::try_parse() {
-        Ok(cli) => cli,
-        Err(e) => return Err(e.into()),
-    };
+    let args = Args::parse();
     let crystal = Crystal::new(args.clone()).await?;
     Supervisor::new(crystal, args.service.recovery_sleep_seconds)
         .start()
