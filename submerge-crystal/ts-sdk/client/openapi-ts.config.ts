de310c45cfee4a497221cf8026e936b3f64366c2
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2026-04-21 22:03:18 +0530

    Add TS client for Crystal APIs (#204)
    * Switched to cursor paging model for the events endpoint in Crystal API. Re #194.
    
    * Some improvements related to cursor paging model for the events endpoint in Crystal API. Re #194.
    
    * feat: ts sdk initialized with crystal client
    
    * chore: remove index.ts
    
    * feat: configured custom eslint and prettier
    
    * chore: regenrate ts-client for crystal
    
    ---------
    
    Co-authored-by: kukabi <kutsalbilgin@gmail.com>

---
+++ submerge-crystal/ts-sdk/client/openapi-ts.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from '@hey-api/openapi-ts';
+
+export default defineConfig({
+    input: 'https://raw.githubusercontent.com/helikon-labs/submerge/refs/heads/development/submerge-crystal/api-spec/submerge-crystal-api.json',
+    output: {
+        format: null,
+        lint: null,
+        path: './src/client',
+        preferExportAll: true,
+    },
+    plugins: [
+        '@hey-api/schemas',
+        {
+            dates: true,
+            name: '@hey-api/transformers',
+        },
+        {
+            enums: 'typescript',
+            name: '@hey-api/typescript',
+        },
+        {
+            name: '@hey-api/sdk',
+            transformer: true,
+            asClass: true,
+            classNameBuilder(name) {
+                return `${name}Service`;
+            },
+            validator: true,
+        },
+        {
+            name: 'zod',
+            requests: true,
+            // FIX: better to enable it but OpenAPI spec file causes validation related issue
+            responses: false,
+        },
+    ],
+});
